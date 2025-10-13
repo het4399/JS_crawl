d2fe8d6ecc59823e8e76a01d43d4c5dedd194423
Het <het.sojitra4399@gmail.com>
2025-10-13 17:38:59 +0530

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx,js,jsx,html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
